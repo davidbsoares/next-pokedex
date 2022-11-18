9bb8c467d92138f69dd33eda179e0898b9e3f288
David Brum <david.brum@meza.digital>
2022-11-18 19:59:02 -0300

    Add Loading Component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="flex w-full max-w-6xl h-[604px] mb-7 justify-center items-center">
+      <img src="/loading.svg" />
+    </div>
+  );
+};
+
+export default Loading;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Pagination from '@components/Pagination';
 import { useState, useEffect } from 'react';
 import { trpc } from 'utils/trpc';
 import Select from '@components/Select';
+import Loading from '@components/Loading';
 
 interface pokemonsListProps {
   pokemons: PokemonProps[];
@@ -26,11 +27,11 @@ const Home: NextPage<pokemonsListProps> = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 9;
 
-  const data = trpc.getPokemons.useQuery({
+  const { data, isLoading } = trpc.getPokemons.useQuery({
     offset: (currentPage - 1) * pageSize,
     types: type.map((t) => t.value),
     kinds: kind.map((t) => t.value),
-  }).data;
+  });
 
   useEffect(() => {
     if (data && total !== data?.[0]) {
@@ -71,11 +72,15 @@ const Home: NextPage<pokemonsListProps> = () => {
           />
         </div>
 
-        <div className="grid items-center justify-center w-full max-w-6xl grid-rows-3 justify-items-center mb-7 grid-cols-auto-fill gap-x-8 gap-y-11">
-          {pokemons.map((pokemon) => (
-            <Card pokemon={pokemon} key={pokemon.id} />
-          ))}
-        </div>
+        {!isLoading ? (
+          <div className="grid items-center justify-center w-full max-w-6xl grid-rows-3 justify-items-center mb-7 grid-cols-auto-fill gap-x-8 gap-y-11">
+            {pokemons.map((pokemon) => (
+              <Card pokemon={pokemon} key={pokemon.id} />
+            ))}
+          </div>
+        ) : (
+          <Loading />
+        )}
 
         {total && (
           <Pagination
